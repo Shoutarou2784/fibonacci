e059f8719b75338c20e54392075b13b91eccf792
Shoutarou2784 <shoutarou2784@gmail.com>
2021-06-09 10:38:30 +0900

    memo化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+function fib(n) {
+    if (n===0) {
+        return 0;
+    }
+    else if (n===1) {
+        return 1;
+    }
+    return fib(n-1) + fib(n-2); //関数の定義にその関数を使うことを再帰という
+}
+const length=40;
+for (let i=0; i<=length;i++) {
+    console.log(fib(i));
+}
